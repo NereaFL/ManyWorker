b124a84899d04726c222a4a7bf82e8db762bdb57
NereaFL <nereafernandezluque@gmail.com>
2024-10-08 13:49:28 +0200

    Inicializacion del Proyecto

---
+++ src/main/java/ManyWorker/ManyWorkerApplication.java
@@ -0,0 +1,13 @@
+package ManyWorker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ManyWorker/ManyWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package ManyWorker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
