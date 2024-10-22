f69db583156aa5d3f7f1a5d9767d87a6262cc6c3
ivanrojano <165648051+ivanrojano@users.noreply.github.com>
2024-10-22 14:22:38 +0200

    Clase Catalogo Garantia

--- src/main/java/ManyWorker/entity/CatalogoGarantia.java
+++ src/main/java/ManyWorker/entity/CatalogoGarantia.java
@@ -1,5 +1,44 @@
 package ManyWorker.entity;
 
-public class CatalogoGarantia {
+import javax.validation.constraints.NotBlank;
 
+import jakarta.persistence.Entity;
+
+@Entity
+public class CatalogoGarantia {
+	 
+	@NotBlank
+	private String titulo;
+	
+	@NotBlank
+	private String terminosLeyes;
+
+	//Contructor vacio
+	public CatalogoGarantia() {
+		super();
+	}
+
+	//Constrcutor con argumentos 
+	public CatalogoGarantia(@NotBlank String titulo, @NotBlank String terminosLeyes) {
+		super();
+		this.titulo = titulo;
+		this.terminosLeyes = terminosLeyes;
+	}
+
+	//Getters and Setters
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTerminosLeyes() {
+		return terminosLeyes;
+	}
+
+	public void setTerminosLeyes(String terminosLeyes) {
+		this.terminosLeyes = terminosLeyes;
+	}
 }
