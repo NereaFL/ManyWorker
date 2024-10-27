f2842aee65c5ffd807dd6d08decc0b533da4cbed
lemaxter <165648200+leMaxter@users.noreply.github.com>
2024-10-27 19:59:21 +0100

    Update Patrocinio.java

--- src/main/java/ManyWorker/entity/Patrocinio.java
+++ src/main/java/ManyWorker/entity/Patrocinio.java
@@ -1,8 +1,37 @@
 package ManyWorker.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Patrocinio extends DomainEntity {
 
+		@NotBlank
+		private String banner;
+		
+		@NotBlank
+		private String enlacePaginaObjetivo;
+
+		// Constructor vacío
+		public Patrocinio() {
+			super();
+		}
+		
+		// Getters y setters
+		public String getBanner() {
+			return banner;
+		}
+
+		public void setBanner(String banner) {
+			this.banner = banner;
+		}
+
+		public String getEnlacePaginaObjetivo() {
+			return enlacePaginaObjetivo;
+		}
+
+		public void setEnlacePaginaObjetivo(String enlacePaginaObjetivo) {
+			this.enlacePaginaObjetivo = enlacePaginaObjetivo;
+		}
+			
 }
