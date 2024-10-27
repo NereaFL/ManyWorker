d4a4eac27ac23df68b4a8d234dbd3372d23df5b6
lemaxter <165648200+leMaxter@users.noreply.github.com>
2024-10-27 19:55:35 +0100

    Update Patrocinador.java

--- src/main/java/ManyWorker/entity/Patrocinador.java
+++ src/main/java/ManyWorker/entity/Patrocinador.java
@@ -1,8 +1,26 @@
 package ManyWorker.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Patrocinador extends Actor {
 
+    // Relación uno a muchos hacia Patrocinio
+    @OneToMany
+    private List<Patrocinio> patrocinios;
+
+    public Patrocinador() {
+        super();
+    }
+
+    // Getters y setters
+    public List<Patrocinio> getPatrocinios() {
+        return patrocinios;
+    }
+
+    public void setPatrocinios(List<Patrocinio> patrocinios) {
+        this.patrocinios = patrocinios;
+    }
 }
