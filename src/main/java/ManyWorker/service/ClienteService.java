3ffc1311dd6e40eb8bbdca30887fe3d0a20644bd
ivanrojano <165648051+ivanrojano@users.noreply.github.com>
2024-11-27 19:08:36 +0100

    Servicio sin modificacion - Cliente

--- src/main/java/ManyWorker/service/ClienteService.java
+++ src/main/java/ManyWorker/service/ClienteService.java
@@ -1,5 +1,13 @@
 package ManyWorker.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ManyWorker.repository.ClienteRepository;
+
+@Service
 public class ClienteService {
 
+	 @Autowired
+	 private ClienteRepository clienteRepository;
 }
