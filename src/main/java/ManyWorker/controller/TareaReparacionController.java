c9bca0d2247d3ff53eb6f62f6cb5dee65c6be292
ivanrojano <165648051+ivanrojano@users.noreply.github.com>
2024-11-27 19:35:10 +0100

    TareaReparacion
    Tarea Reparacion Controller - Tarea Reparacion Service

--- src/main/java/ManyWorker/controller/TareaReparacionController.java
+++ src/main/java/ManyWorker/controller/TareaReparacionController.java
@@ -1,5 +1,30 @@
 package ManyWorker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ManyWorker.entity.TareaReparacion;
+import ManyWorker.service.TareaReparacionService;
+
+@RestController
+@RequestMapping("/tareaReparacion")
 public class TareaReparacionController {
 
+	@Autowired
+    private TareaReparacionService tareaReparacionService;
+	
+	@PostMapping("/crear")
+    public TareaReparacion crearTareaReparacion(@RequestBody TareaReparacion tareaReparacion) {
+        return tareaReparacionService.crearTareaReparacion(tareaReparacion);
+    }
+	
+	@DeleteMapping("/eliminar/{id}")
+    public void eliminarTareaReparacion(@PathVariable int id) {
+		tareaReparacionService.eliminarTareaReparacion(id);
+    }
 }
